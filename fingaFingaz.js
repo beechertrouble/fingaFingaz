f257f763d77f562cae0e3361c13d3524cc6ae0b4
Avery Brooks <averyjamesbrooks@gmail.com>
2015-02-02 21:46:07 -0500

    amd hotfix (return ref from inner scope to make IE8 shut up)

--- fingaFingaz.js
+++ fingaFingaz.js
@@ -12,11 +12,11 @@
 	 */
 	if (W.fingaFingaz)
 		return;
-	
+
 	$ = $;
-	
+
 	var fingaFingaz = (function() {
-	
+
 		var FF = {
 				inited : false,
 				start : {x : 0, y : 0},
@@ -43,117 +43,117 @@
 			init, touchStarter, touchMover, touchEnder, getZoom, gestureStarter, gestureEnder,
 			swipeup, swiperight, swipedown, swipeleft,
 			swipeUpHandler, swipeRightHandler, swipeDownHandler, swipeLeftHandler, polyfill;
-				
+
 		FF.getCoords = function(e) {
-	
+
 			return { x : e.pageX !== undefined ? e.pageX : e.originalEvent.pageX, y : e.pageY !== undefined ? e.pageY : e.originalEvent.pageY };
-			
+
 		}; //
 		FF.trigger = function(e, ev) {
 
 			D.dispatchEvent(ev);
 			if($ !== undefined)
 				$(e.target).trigger(ev.type);
-						
+
 		}; //
 		FF.init = function($) {
-			
+
 			if(FF.inited) return;
 			FF.inited = true;
-				
+
 			if(!supportsTouch || !D.addEventListener) return;
-							
+
 			// add listeners and custom events ...
 			D.addEventListener("touchstart", touchStarter, false);
 			D.addEventListener("touchmove", touchMover, false);
 			D.addEventListener("touchend", touchEnder, false);
 			D.addEventListener('gesturestart', gestureStarter, false);
 			D.addEventListener('gestureend', gestureEnder, false);
-			
+
 			if(W.navigator.msPointerEnabled) {
-				
+
 				D.addEventListener("MSPointerDown", touchStarter, false);
 				D.addEventListener("MSPointerMove", touchMover, false);
 				D.addEventListener("MSPointerUp", touchEnder, false);
-							
+
 			}
-			
+
 			// swipes ...
 			if(polyfilled)
 				polyfill();
-			
+
 			swipeup = new CustomEvent('swipeup');
 			swiperight = new CustomEvent('swiperight');
-			swipedown = new CustomEvent('swipedown'); 
+			swipedown = new CustomEvent('swipedown');
 			swipeleft = new CustomEvent('swipeleft');
 			D.addEventListener('swipeup', swipeUpHandler, false);
 			D.addEventListener('swiperight', swipeRightHandler, false);
 			D.addEventListener('swipedown', swipeDownHandler, false);
 			D.addEventListener('swipeleft', swipeLeftHandler, false);
-			
+
 			// add to jq
 			$ = $ !== undefined ? $ : W.jQuery;
 			if($ !== undefined)
 				$.event.props.push(['touchstart', 'touchmove', 'touchend', 'gesturestart', 'gestureend', 'swipeup', 'swiperight', 'swipedown', 'swipeleft']);
-						
+
 			return FF;
-				
+
 		};
 		//
 		touchStarter = function(e) {
-			
+
 			if(polyfilled && e.pointerType != 'touch') return;
-			
+
 			var c = FF.getCoords(e);
-			
+
 			FF.fingers = 1;
 			FF.start.y = c.y;
 			FF.start.x = c.x;
-			FF.prevPos.y = FF.start.y * 1;	
-			FF.prevPos.x = FF.start.x * 1;	
-			FF.pos.y = FF.start.y * 1;	
-			FF.pos.x = FF.start.x * 1;	
+			FF.prevPos.y = FF.start.y * 1;
+			FF.prevPos.x = FF.start.x * 1;
+			FF.pos.y = FF.start.y * 1;
+			FF.pos.x = FF.start.x * 1;
 			FF.startTime = new Date().getTime();
-									
+
 		} //
 		touchMover = function(e) {
-					
+
 			if(polyfilled && e.pointerType != 'touch') return;
-			
+
 			var c = FF.getCoords(e);
-			
-			if(c.y === undefined) return true;		
-	
-			FF.pos.y = c.y;	
+
+			if(c.y === undefined) return true;
+
+			FF.pos.y = c.y;
 			FF.pos.x = c.x;
 			FF.step.y = FF.prevPos.y - FF.pos.y;
 			FF.step.x = FF.prevPos.x - FF.pos.x;
 			FF.prevPos.y = FF.pos.y * 1;
 			FF.prevPos.x = FF.pos.x * 1;
-			
+
 		} //
 		touchEnder = function(e) {
-						
+
 			if(polyfilled && e.pointerType != 'touch') return;
-			
+
 			FF.endTime = new Date().getTime();
 			FF.duration = FF.endTime - FF.startTime;
-	
+
 			// e.preventDefault();
-			// single finger jams				
-			if(FF.fingers < 2) { 
-	
+			// single finger jams
+			if(FF.fingers < 2) {
+
 				if(FF.firstTouch)
 					FF.firstTouch = false;
-	
+
 				// distance
 				FF.dist.y = FF.start.y - FF.pos.y;
 				FF.dist.x = FF.start.x - FF.pos.x;
-	
+
 				// swipe / tap
 				FF.tap = Math.abs(FF.dist.x) > FF.threshold && Math.abs(FF.dist.y) > FF.threshold ? true : false;
 				FF.swipe = FF.tap ? false : true;
-								
+
 				// swipe
 				if(Math.abs(FF.dist.x) > FF.threshold) {
 					FF.swipeH = FF.dist.x > 0 ? 'left' : 'right';
@@ -174,17 +174,17 @@
 				} else {
 					FF.swipeV = false;
 				}
-	
+
 			}
-	
-			/* reset	
+
+			/* reset
 			FF.pos.y = 0;
 			FF.pos.x = 0;
 			*/
-					
+
 		} //
-		getZoom = function() { 
-			return W.outerWidth / W.innerWidth; 
+		getZoom = function() {
+			return W.outerWidth / W.innerWidth;
 		} //
 		gestureStarter = function(e) {
 			FF.fingers = 2;
@@ -197,7 +197,7 @@
 		} //
 		swipeRightHandler = function(e) {
 			//console.log('swipeRight');
-		} // 
+		} //
 		swipeDownHandler = function(e) {
 			//console.log('swipeDown');
 		} //
@@ -205,7 +205,7 @@
 			//console.log('swipeLeft');
 		} //
 		polyfill = function() {
-			
+
 			(function () {
 				function CustomEvent( event, params ) {
 					params = params || { bubbles: true, cancelable: false, detail: undefined };
@@ -213,27 +213,27 @@
 					evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
 					return evt;
 				};
-				
+
 				CustomEvent.prototype = W.Event.prototype;
 				W.CustomEvent = CustomEvent;
 			})();
-					
+
 		}
-		
+
 		return FF;
-		
+
 	})();
-	
+
 	/**
 	 * Apply the ikelos function to the supplied scope (window)
 	 */
 	W.fingaFingaz = fingaFingaz;
 
-})(jQuery || $, window);
+	/**
+	* Expose fingaFingaz as an AMD, return local fingaFingaz reference
+	*/
+	if (typeof define === "function") {
+		define("fingaFingaz", [], function () { return fingaFingaz; } );
+	}
 
-/**
- * Expose fingaFingaz as an AMD
- */
-if (typeof define === "function") {
-	define("fingaFingaz", [], function () { return fingaFingaz; } );
-}
+})(jQuery || $, window);
